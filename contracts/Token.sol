fc472b47d97de9d3ddec23bdf0f7ce0ebcdc6615
Dilan Tompkins <dilantompkins@gmail.com>
2021-08-09 18:11:28 -0400

    first commit

---
+++ contracts/Token.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "hardhat/console.sol";
+import "/Users/tomterrific/Projects/token-react/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract WarEagleCoin is ERC20 {
+    constructor() ERC20("WarEagleCoin", "WDE") {
+        _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+}
